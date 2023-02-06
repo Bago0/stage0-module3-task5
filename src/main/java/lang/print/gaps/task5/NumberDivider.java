43726470c1d6a4f2dee374bddbf9525ebcfce10f
Bago0 <jgamadzebagrati@gmail.com>
2023-02-06 21:03:33 +0400

    exercise 6

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int divideByFive = toBeDivided / 5;
+        System.out.println(divideByFive);
     }
 }
